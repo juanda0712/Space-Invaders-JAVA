4fa05401e57ffe585f2061e88c4d481303668086
Jose Umaña <jcur1802@gmail.com>
2021-04-22 13:55:49 -0600

    Se agrego metodo addInvaders

--- src/main/java/rows/implementations/rows/Basic.java
+++ src/main/java/rows/implementations/rows/Basic.java
@@ -1,13 +1,26 @@
 package main.java.rows.implementations.rows;
 
+import main.java.invaders.InvadersFactory;
+import main.java.invaders.interfaces.Invader;
 import main.java.lists.implementations.list.LinkedList;
-import main.java.lists.interfaces.IList;
 import main.java.rows.interfaces.IRow;
 
 public class Basic implements IRow {
     @Override
     public void creaLista(){
-        IList lista = new LinkedList();
+        LinkedList lista = new LinkedList();
+        this.addInvaders(6, lista);
         lista.pruebaFuncionamiento();
     }
+
+    public void addInvaders(int cant, LinkedList list){
+        InvadersFactory factory1 = new InvadersFactory();
+        while (cant != 0) {
+            Invader newInvader = factory1.createInvader("NORMAL");
+            list.append("1");
+            cant -= 1;
+        }
+    }
+
 }
+
--- src/main/java/rows/implementations/rows/TypeB.java
+++ src/main/java/rows/implementations/rows/TypeB.java
@@ -1,15 +1,35 @@
 package main.java.rows.implementations.rows;
 
+import main.java.invaders.InvadersFactory;
+import main.java.invaders.interfaces.Invader;
 import main.java.lists.implementations.list.DoublyLinkedList;
-import main.java.lists.implementations.list.LinkedList;
-import main.java.lists.interfaces.IList;
 import main.java.rows.interfaces.IRow;
 
 public class TypeB implements IRow {
 
     @Override
     public void creaLista(){
-        IList lista = new DoublyLinkedList();
+        DoublyLinkedList lista = new DoublyLinkedList();
+        int min = 0;
+        int max = 6;
+        int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
+        this.addInvaders(7, lista, random_int);
         lista.pruebaFuncionamiento();
+    }
+    public void addInvaders(int cant, DoublyLinkedList list, int bossPos) {
+        InvadersFactory factory1 = new InvadersFactory();
+        int pos = 0;
+        System.out.println(bossPos);
+        while (pos != cant) {
+            Invader newInvader;
+            if (pos == bossPos) {
+                newInvader = factory1.createInvader("BOSS");
+                list.append("boss");
+            } else {
+                newInvader = factory1.createInvader("NORMAL");
+                list.append("normal");
+            }
+            pos += 1;
+        }
     }
 }
